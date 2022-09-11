13e82bf5c9a99ba873e045fc4625cf5cd22e6e31
不枝雀 <yunxu1019@qq.com>
2022-09-11 11:19:57 +0800

    编译方法更新

--- coms/main.js
+++ coms/main.js
@@ -1,6 +1,15 @@
-var [, , pngname, scale] = process.argv;
-if (/^(\d+)?(\.\d*)?(p[xt]|r?em|[cm]m)?$/i.test(pngname)) {
-    scale = pngname;
-    pngname = "";
+var concat = require("./concat");
+var pngname, scale, prefix;
+for (var arg of process.argv.slice(2)) {
+    if (/^(\d+)?(\.\d*)?(p[xt]|r?em|[cm]m)?$/i.test(arg)) {
+        scale = arg;
+    }
+    else if (/\-$/.test(arg)) {
+        prefix = arg;
+    }
+    else if (!pngname) {
+        pngname = arg;
+    }
+
 }
-concat(process.cwd(), pngname || "png-concat.concat", scale);
+concat(process.cwd(), pngname || prefix && prefix.replace(/\-$/, '') || "png-concat.concat", scale, prefix);
--- tools/build.bat
+++ tools/build.bat
@@ -1,9 +1,8 @@
 @echo off
 setlocal
 set app=concat.js
-call efront publish exports
+call efront build --export_to=exports
 move public\comm\concat public\concat
 echo #!/usr/bin/env node>public\main
-echo var concat = require("./concat");>>public\main
 type coms\main.js>>public\main
 rd /s /q public\comm
