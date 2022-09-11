04b24c83e498bfa526bfd3cd450c3a244a6cdb4d
不枝雀 <yunxu1019@qq.com>
2022-09-11 11:25:44 +0800

    编译方法更新

--- _envs/setup.bat
+++
@@ -1,3 +0,0 @@
-set PAGE_PATH=./apps/
-set COMS_PATH=./coms,:
-set comm=./,zimoli,reptile
--- tools/build.bat
+++ tools/build.bat
@@ -1,8 +1,6 @@
 @echo off
 setlocal
 set app=concat.js
-call efront build --export_to=exports
-move public\comm\concat public\concat
+call efront build --export_to=exports --release
 echo #!/usr/bin/env node>public\main
 type coms\main.js>>public\main
-rd /s /q public\comm
